12ff81b1b600563019c4606beedc351198a82027
vitorcx <vitorcx010@gmail.com>
2018-06-20 05:21:23 -0300

    Corrigindo comandos no texto de ajuda

--- taskbot.py
+++ taskbot.py
@@ -9,11 +9,11 @@ import db
 from db import Task
 
 HELP = """
- /new NOME
+ /new NOME, NOME...
  /todo ID
  /doing ID
  /done ID
- /delete ID
+ /delete ID, ID...
  /list
  /rename ID NOME
  /dependson ID ID...
